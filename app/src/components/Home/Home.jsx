7711b496c8deabd6b0732b8e655479a1db926d0b
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2021-04-23 16:43:44 +0530

    Updates

--- app/src/components/Home/Home.jsx
+++ app/src/components/Home/Home.jsx
@@ -1,7 +1,12 @@
 import "../../styles/Home.css";
+import fb from "../../service/firebase";
 
 function Home() {
-  return <div className="home-container">Home</div>;
+  return (
+    <div className="home-container">
+      Home{fb.firestore.collection("gym").doc()}
+    </div>
+  );
 }
 
 export default Home;

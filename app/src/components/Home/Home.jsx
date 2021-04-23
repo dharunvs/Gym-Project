3a7e3bc71686e852175de34b5440885c4288f2e9
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2021-04-23 11:27:40 +0530

    Updates

--- app/src/components/Home/Home.jsx
+++ app/src/components/Home/Home.jsx
@@ -1,5 +1,7 @@
+import "../../styles/Home.css";
+
 function Home() {
-  return <div className="auth-form-container">Home</div>;
+  return <div className="home-container">Home</div>;
 }
 
 export default Home;
